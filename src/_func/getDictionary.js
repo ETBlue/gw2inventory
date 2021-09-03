b5cb1abb865cb8f9d1c4cff0020e677a4e68a523
ETBlue <etblue.tw@gmail.com>
2021-09-04 07:07:36 +0800

    env: switch to Chakra - React - TypeScript combo

--- src/_func/getDictionary.js
+++
@@ -1,9 +0,0 @@
-const getDictionary = (array, key = 'id') => {
-  const result = {}
-  for (const item of array) {
-    result[item[key]] = item
-  }
-  return result
-}
-
-export default getDictionary
--- src/_func/trimPath.js
+++
@@ -1,6 +0,0 @@
-const trimPath = ({level, path}) => {
-  const segments = path.split('/').filter(item => item.length !== 0)
-  return `/${segments.slice(0, level).join('/')}`
-}
-
-export default trimPath
