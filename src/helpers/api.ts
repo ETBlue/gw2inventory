362e547ddf1ba591935dafd9289c54d9c0bd9b20
ETBlue <etblue.tw@gmail.com>
2021-09-11 16:48:38 +0800

    infra: queryFunction - support params

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -4,8 +4,13 @@ import { API_URL } from "config"
 
 export const queryFunction = async (context: QueryFunctionContext) => {
   const { queryKey } = context
-  const [endpoint, token] = queryKey
-  const res = await fetch(`${API_URL}/${endpoint}?access_token=${token}`)
+  const [endpoint, token = "", params = ""] = queryKey
+
+  const searchParams = new URLSearchParams(params)
+  searchParams.append("access_token", token)
+  const queryString = searchParams.toString()
+
+  const res = await fetch(`${API_URL}/${endpoint}?${queryString}`)
   if (res.ok) {
     const data = await res.json()
     return data
