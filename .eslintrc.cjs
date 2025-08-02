3919e712ae096294bf95bd3022c66a70d7776b7b
ETBlue <etblue.tw@gmail.com>
2025-08-02 19:37:30 +0800

    chore: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // Must be last
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    // React specific
+    'react/prop-types': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    
+    // TypeScript specific
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    
+    // Prettier integration
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'dist',
+    'build',
+    'node_modules',
+    'coverage',
+    '*.config.js',
+    '*.config.ts',
+  ],
+}
